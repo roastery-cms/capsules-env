b839c339aa7e6f127d53577e57f04e496273d509
Alan Reis <alanreisanjo@gmail.com>
2026-03-23 11:52:51 -0300

    chore: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,33 @@ import { barista } from "@roastery/barista";
 import type Elysia from "elysia";
 
 export function baristaEnv<ContentType extends t.TObject[]>(
-    ...args: ContentType
+	...args: ContentType
 ): Elysia<
-    "",
-    {
-        decorator: {
-            env: t.Static<t.TComposite<ContentType>> & ServerDependenciesDTO;
-        };
-        store: Record<string, unknown>;
-        derive: Record<string, unknown>;
-        resolve: Record<string, unknown>;
-    }
+	"",
+	{
+		decorator: {
+			env: t.Static<t.TComposite<ContentType>> & ServerDependenciesDTO;
+		};
+		store: Record<string, unknown>;
+		derive: Record<string, unknown>;
+		resolve: Record<string, unknown>;
+	}
 > {
-    type EnvType = t.Static<t.TComposite<ContentType>> & ServerDependenciesDTO;
+	type EnvType = t.Static<t.TComposite<ContentType>> & ServerDependenciesDTO;
 
-    args.push(ServerDependenciesDTO);
+	args.push(ServerDependenciesDTO);
 
-    const envDTO = t.Composite<ContentType>(args);
-    const envContent: EnvType = Object.fromEntries(
-        Object.entries(envDTO.properties).map(([key, _]) => [
-            key,
-            process.env[key],
-        ]),
-    ) as never;
+	const envDTO = t.Composite<ContentType>(args);
+	const envContent: EnvType = Object.fromEntries(
+		Object.entries(envDTO.properties).map(([key, _]) => [
+			key,
+			process.env[key],
+		]),
+	) as never;
 
-    if (!Schema.make(envDTO).match(envContent)) {
-        throw new InvalidEnvironmentException("system@boot");
-    }
+	if (!Schema.make(envDTO).match(envContent)) {
+		throw new InvalidEnvironmentException("system@boot");
+	}
 
-    return barista().as("global").decorate("env", envContent);
+	return barista().as("global").decorate("env", envContent);
 }
